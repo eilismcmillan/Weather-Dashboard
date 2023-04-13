ce7d9fb3336a1b0a493cafa8de643203ba6c9541
eilismcmillan <eilis.mcmillan1998@gmail.com>
2023-04-13 20:13:14 +1000

    updated js and readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Weather-Dashboard
+
+## Description
+This website enables user to search for cities all over the world, and obtain both the current day's weather, and a 5 day forecast for that city. 
+
+## Usage
+By using the search bar on the left hand side of the screen, users may enter a city of their choosing and press search. Upon doing so, the current day's weather is displayed as well as the 5-day forecast below. The cities searched are added to a list on the side. 
+
+![screenshot of weather dashboard](assets/images/weather dashboard screenshot.png)
+
+## Credits
+https://openweathermap.org/forecast5 
+https://openweathermap.org/api/geocoding-api
+https://openweathermap.org/weather-conditions
+
+## License
+No license
--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,8 @@ var searchSubmit = function (event) {
   event.preventDefault();
 
   var city = cityInput.value.trim();
-  localStorage.setItem("city", city);
+
+  localStorage.setItem("searched-city", city);
 
   if (city) {
     getCoords(city);
@@ -13,6 +14,7 @@ var searchSubmit = function (event) {
   } else {
     alert("Please enter a City");
   }
+  displayCities();
 };
 
 var getCoords = function (city) {
@@ -23,7 +25,6 @@ var getCoords = function (city) {
 
   fetch(geoUrl)
     .then(function (response) {
-      console.log(response);
       if (response.ok) {
         response.json().then(function (data) {
           var latitude = data[0].lat;
@@ -45,22 +46,20 @@ var getForecast = function (latitude, longitude) {
     latitude +
     "&lon=" +
     longitude +
-    "&appid=b913321cbeef3ffe610df59d6b749231";
+    "&units=metric&appid=b913321cbeef3ffe610df59d6b749231";
   fetch(weatherUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-            console.log(data)
-            let forecastArray = []
-        for (let i = 0; i < data.list.length; i++) {
+          let forecastArray = [];
+          for (let i = 0; i < data.list.length; i++) {
             const forecastObject = data.list[i];
-            const testTime = forecastObject.dt_txt.split(" ")[1]
-            if(testTime==="12:00:00") {
-                forecastArray.push(forecastObject)
+            const testTime = forecastObject.dt_txt.split(" ")[1];
+            if (testTime === "12:00:00") {
+              forecastArray.push(forecastObject);
             }
-            
-        }
-        displayForecast(forecastArray, data.name)
+          }
+          displayForecast(forecastArray);
         });
       } else {
         alert("Error: " + response.statusText);
@@ -71,68 +70,83 @@ var getForecast = function (latitude, longitude) {
     });
 };
 
-var getWeather= function (latitude, longitude) {
-    var weatherUrl =
-      "http://api.openweathermap.org/data/2.5/weather?lat=" +
-      latitude +
-      "&lon=" +
-      longitude +
-      "&units=metric&appid=b913321cbeef3ffe610df59d6b749231";
-    fetch(weatherUrl)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-              console.log(data)
-            displayWeather(data, data.name);
-          });
-        } else {
-          alert("Error: " + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert("Unable to connect to Weatherorg");
-      });
-      getForecast(latitude,longitude)
-  };
-
-var displayForecast = function(array) {;
-    array.forEach(function(day){
-        var cardDiv = document.createElement("div");
-        var cardH4 = document.createElement("h3");
-        var tempH4 = document.createElement("h4");
-        var humidityH4 = document.createElement("h4");
-        var windH4 = document.createElement("h4");
-        var dateArr = day.dt_txt.split(" ")[0].split("-");
-        var date = dateArr[2] + "/" + dateArr[1];
-        cardH4.textContent = date;
-        tempH4.textContent = "temperature: " + data.main.temp + "°C";
-        humidityH4.textContent = "humidity: " + data.main.humidity + "%";
-        windH4.textContent = "wind: " + data.wind.speed + "km/h";
-        cardDiv.appendChild(cardH4);
-        document.getElementById("day-container").appendChild(cardDiv);
-
+var getWeather = function (latitude, longitude) {
+  var weatherUrl =
+    "http://api.openweathermap.org/data/2.5/weather?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    "&units=metric&appid=b913321cbeef3ffe610df59d6b749231";
+  fetch(weatherUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          displayWeather(data, data.name);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to Weatherorg");
     });
-        
-    
-}
-
-var displayWeather = function (data, city) {
-    console.log(data, city)
-    //var date = data.dt_txt.split(" ")[0]
-    document.querySelector(".city-name").textContent= city
-    document.querySelector(".city-temp").textContent = "temperature: " + data.main.temp + "°C"
-    document.querySelector(".city-humidity").textContent = "humidity: " + data.main.humidity + "%"
-    document.querySelector(".city-wind").textContent = "wind: " + data.wind.speed + "km/h"
+  getForecast(latitude, longitude);
 };
 
-var displayCities = function (event) {
-  event.preventDefault();
+var displayForecast = function (array) {
+  var container = document.getElementById("day-container");
+  container.textContent = "";
+  array.forEach(function (day) {
+    var cardDiv = document.createElement("div");
+    cardDiv.setAttribute("id", "weather-card");
+    var cardH4 = document.createElement("h3");
+    var tempH4 = document.createElement("h4");
+    var humidityH4 = document.createElement("h4");
+    var windH4 = document.createElement("h4");
+    var imageEl = document.createElement("img");
+    var dateArr = day.dt_txt.split(" ")[0].split("-");
+    var date = dateArr[2] + "/" + dateArr[1];
+    imageEl.src =
+      "https://openweathermap.org/img/wn/" + day.weather[0].icon + "@2x.png";
+    cardH4.textContent = date;
+    tempH4.textContent = "temperature: " + day.main.temp + "°C";
+    humidityH4.textContent = "humidity: " + day.main.humidity + "%";
+    windH4.textContent = "wind: " + day.wind.speed + "km/h";
+    cardDiv.append(cardH4, imageEl, tempH4, humidityH4, windH4);
+    document.getElementById("day-container").appendChild(cardDiv);
+  });
+};
+
+var displayWeather = function (data, city) {
+  var iconDiv = document.getElementById("weather-icon");
+  iconDiv.textContent = "";
+  var dayImage = document.createElement("img");
+  dayImage.src =
+    "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+  iconDiv.append(dayImage);
+  document.querySelector(".city-name").textContent =
+    "Today's Forecast for " + city;
+  document.querySelector(".city-temp").textContent =
+    "temperature: " + data.main.temp + "°C";
+  document.querySelector(".city-humidity").textContent =
+    "humidity: " + data.main.humidity + "%";
+  document.querySelector(".city-wind").textContent =
+    "wind: " + data.wind.speed + "km/h";
+};
+
+var displayCities = function () {
+  var savedCity = localStorage.getItem("searched-city");  
   var cityBtn = document.createElement("button");
-  cityBtn.textContent = city;
+  cityBtn.setAttribute("id", "city-button");
+  cityBtn.textContent = savedCity;
   var cityLiEL = document.createElement("li");
   var cityList = document.getElementById("city-list");
   cityLiEL.append(cityBtn);
   cityList.appendChild(cityLiEL);
+
+  cityBtn.addEventListener("click", getCoords(savedCity))
 };
 
 searchForm.addEventListener("submit", searchSubmit);
+
